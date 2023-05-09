91119fe77bdaee2ae109e6c3cdeed7ad6a70b7db
albim72 <marcin.albiniak72@gmail.com>
2023-05-09 11:25:37 +0200

    Update cdklasa.py

--- DZIEN_1/kolekcje_moduly/klasazbiorcza/cdklasa.py
+++ DZIEN_1/kolekcje_moduly/klasazbiorcza/cdklasa.py
@@ -1,10 +1,10 @@
 from samochodowe_funkcje.sfunckje import *
 
 class CDDane:
-    #fukcja konstrukcyjna new tworzy definicję klasy
-    def __new__(cls, *args, **kwargs):
-        pass
 
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(CDDane)
+    
     # fukcja konstrukcyjna init tworzy definicję obiektu
     def __init__(self,lista,slownik):
         self.lista = lista
@@ -15,3 +15,4 @@ class CDDane:
 
     def czytaj_s(self):
         return czytaj_sam(self.slownik)
+
