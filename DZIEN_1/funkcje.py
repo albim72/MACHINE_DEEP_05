a48a24fe1eb4084e049c962b8a712895aca497d1
albim72 <marcin.albiniak72@gmail.com>
2023-05-09 10:33:20 +0200

    Update funkcje.py

--- DZIEN_1/funkcje.py
+++ DZIEN_1/funkcje.py
@@ -10,3 +10,24 @@ def osoba(funkcja,*args):
 print(osoba(witaj,"Leon"))
 
 print(osoba(konkurs,"Olga",78))
+
+#funkcja wrapper
+
+def startstop(funkcja):
+    def wrapper(*args):
+        print("Startowanie procesu...")
+        funkcja(*args)
+        print("Kończenie procesu")
+    return wrapper
+
+def zawijanie(czego):
+    print(f"zawijanie {czego} w sreberka")
+
+zw = startstop(zawijanie)
+zw("czekoladek")
+
+@startstop
+def dmuchanie(czego):
+    print(f"dmuchanie {czego} w urodziny")
+
+dmuchanie("świeczek")
