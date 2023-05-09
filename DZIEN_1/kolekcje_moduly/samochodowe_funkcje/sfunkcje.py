4e7bedba780c0fbeca4aefb05d107deb2815c921
albim72 <marcin.albiniak72@gmail.com>
2023-05-09 11:06:36 +0200

    Create sfunkcje.py

---
+++ DZIEN_1/kolekcje_moduly/samochodowe_funkcje/sfunkcje.py
@@ -0,0 +1,8 @@
+def czytaj_lata(lista):
+    for i,j in enumerate(lista):
+        print(f"Element listy nr {i+1} i jego wartość: {j}.")
+
+def czytaj_sam(slownik):
+    for x,y in slownik.items():
+        print(f"klucz: {x}, wartość: {y}")
+
