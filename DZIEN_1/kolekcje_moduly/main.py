28e0213dd34ecbff5bc220516675845b01f9ab73
albim72 <marcin.albiniak72@gmail.com>
2023-05-09 11:26:05 +0200

    Update main.py

--- DZIEN_1/kolekcje_moduly/main.py
+++ DZIEN_1/kolekcje_moduly/main.py
@@ -5,6 +5,8 @@ from dane import samochod as sam
 
 from samochodowe_funkcje.sfunckje import czytaj_lata,czytaj_sam
 
+from klasazbiorcza.cdklasa import CDDane
+
 
 print("_________ wywołanie bezpośrednie ________")
 print(rok)
@@ -13,3 +15,8 @@ print(sam)
 print("_________ wywołanie przez funkcje ________")
 czytaj_lata(rok)
 czytaj_sam(sam)
+
+print("_________ wywołanie przez obiekt ________")
+cd = CDDane(rok,sam)
+cd.czytaj_l()
+cd.czytaj_s()
