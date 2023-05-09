8dbe062dd86b88463b986ca0f3cd40b308237336
albim72 <marcin.albiniak72@gmail.com>
2023-05-09 10:25:02 +0200

    Create funkcje.py

---
+++ DZIEN_1/funkcje.py
@@ -0,0 +1,12 @@
+def witaj(imie):
+    return f"Miło Cię widzieć {imie}."
+
+def konkurs(imie,punkty):
+    return f"uczestnik konkursu {imie}, liczba punktów: {punkty}"
+
+def osoba(funkcja,*args):
+    return funkcja(*args)
+
+print(osoba(witaj,"Leon"))
+
+print(osoba(konkurs,"Olga",78))
