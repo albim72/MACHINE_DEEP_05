140febcf46794c9b905c33a7e50eea813786c053
albim72 <marcin.albiniak72@gmail.com>
2023-05-11 11:49:34 +0200

    Create olivetti_faces.py

---
+++ DZIEN_3/olivetti_faces.py
@@ -0,0 +1,30 @@
+import sklearn
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.ensemble import RandomForestClassifier
+import time
+import matplotlib.pyplot as plt
+
+
+n_jobs = -1
+data = sklearn.datasets.fetch_olivetti_faces()
+X,y = data.data, data.target
+
+mask = y<5
+X = X[mask]
+y=y[mask]
+
+forest = RandomForestClassifier(n_estimators=750,n_jobs=n_jobs, random_state=42)
+forest.fit(X,y)
+
+start_time = time.time()
+img_shape = data.images[0].shape
+importances = forest.feature_importances_
+elapsed_time = time.time() - start_time
+
+print(f"czas obliczenia istotnych pikseli: {elapsed_time:.3f} s")
+imp_reshaped = importances.reshape(img_shape)
+
+plt.matshow(imp_reshaped,cmap=plt.cm.hot)
+plt.title("Istotne piksele obliczone przy użyciu wartości zanieczyszczeń.")
+plt.colorbar()
+plt.show()
